843a42da05d49a149c1fd035e955315e93a9556a
Tony Mugendi <mugendi.tony26@gmail.com>
2017-10-16 20:43:31 +0300

    Updates

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,6 +3,12 @@ from restaurants.views import *
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^restaurants/$', restaurant_list),
+	url(r'^restaurants/$', RestaurantListView.as_view()),
+    url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
+    url(r'^restaurants/(?P<pk>\w+)/$', RestaurantDetailView.as_view()),
+
+    # url(r'^restaurants/african/$', AfricanRestaurantListView.as_view()),
+    # url(r'^restaurants/brazillian/$', BrazillianRestaurantListView.as_view()),
+
     
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import *
 
 # Create your views here.
@@ -11,3 +12,21 @@ def restaurant_list(request):
 		'object_list': queryset
 	}
 	return render(request, template_name, context)
+
+class RestaurantListView(ListView):
+	def get_queryset(self):
+		slug = self.kwargs.get('slug')
+		if slug:
+			queryset = Restaurant.objects.filter(
+					Q(category__iexact=slug) |
+					Q(category__icontains=slug)
+				)
+		else:
+			queryset = Restaurant.objects.all()
+
+		return queryset
+
+class RestaurantDetailView(DetailView):
+	queryset = Restaurant.objects.all()
+
+
