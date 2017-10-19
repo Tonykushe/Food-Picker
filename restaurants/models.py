1027df7eba5714f3ef48bad216bbf64f5a8252bc
Tony Mugendi <mugendi.tony26@gmail.com>
2017-10-19 17:56:21 +0300

    Slugs

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,8 +5,13 @@ class Restaurant(models.Model):
 	name 		= models.CharField(max_length=120)
 	location 	= models.CharField(max_length=120, null=True, blank=True)
 	category 	= models.CharField(max_length=120, null=True, blank=True)
+	timestamp	= models.DateTimeField(auto_now=True)
+	updated  	= models.DateTimeField(auto_now=True)
+	slug  		= models.SlugField(null=True, blank=True)
 
 	def __str__(self):
 		return self.name
 
-
+	@property
+	def title(self):
+		return self.name
